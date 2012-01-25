2bf651b71fc543d519154dd7f9b35f5db73f994c
Andy Gill <andygill@ku.edu>
2012-01-25 11:32:17 -0600

    First cut at git repo for testing framework.

---
+++ src/Diagrams/Tests.hs
@@ -0,0 +1,79 @@
+{-# LANGUAGE FlexibleContexts, RankNTypes, NoMonomorphismRestriction #-}
+
+module Diagrams.Tests
+        ( Test(..)
+        , runTests
+        , examples
+        ) where
+
+import Diagrams.Prelude
+import Text.Html as H
+
+-----------------------------------------------------------------------
+
+data Test = Test
+        String -- ^ the name of the test
+        (forall canvas .
+                ( Renderable (Path R2) canvas
+                , Backend canvas R2
+                ) => Diagram canvas R2
+        ) -- ^ and the diagram
+
+-----------------------------------------------------------------------
+
+-- | 'runTests' generates an HTML page which contains the executed tests.
+-- You need to provide the tests to run (typically 'examples'),
+-- the name of the file to generate, and generating an HTML fragment
+-- that displays your image.
+
+runTests ::  [Test] -> String -> (Test -> IO Html) -> IO ()
+runTests tests name exec = do
+        let header = tr
+                $ concatHtml [ th << "(name)"
+                             , th << "expected output"
+                             , th << "diagram under test"
+                             ]
+        result_rows <- sequence
+                [ do inside <- exec t
+                     let golden = H.image ! [src ("expected_" ++ nm ++ ".png")]
+                     return
+                        $ tr
+                        $ concatHtml [ td ! [valign "top", bgcolor "#eeeeee"]  $ toHtml nm
+                                     , td $ golden
+                                     , td $ inside
+                                     ]
+                | t@(Test nm _) <- tests
+                ]
+        let doc = body
+                $ table
+                $ concatHtml (header : result_rows)
+        writeFile name $ renderHtml doc
+
+-- ^ list of cannonical examples.
+examples :: [Test]
+examples =
+        [ Test "square1" $ square 1
+        , Test "circle1" $ circle 1
+        , Test "circle-square" $
+                circle 1 ||| square 1
+        , Test "2-circles" $
+                circle 0.5 <> unitCircle
+        , Test "ellipse" $
+                unitCircle # scaleX 0.5 # rotateBy (1/6)
+        , Test "arc" $
+                arc (tau/4 :: Rad) (4 * tau / 7 :: Rad)
+        , Test "Pre-defined-shapes" $
+                square 1 ||| rect 0.3 0.5 ||| eqTriangle 1 ||| roundedRect (0.7,0.4) 0.1
+        , Test "circle-hrule-circle" $
+                circle 1 ||| hrule 2 ||| circle 1
+        , Test "poly-example" $
+                poly_example
+        ]
+
+poly_example = (poly1 ||| strutX 1 ||| poly2) # lw 0.05
+  where
+          poly1 = polygon with { polyType   = PolyRegular 13 5
+                               , polyOrient = OrientV }
+          poly2 = polygon with { polyType   = PolyPolar (repeat (1/40 :: CircleFrac))
+                                       (take 40 $ cycle [2,7,4,6]) }
+
