4693c1233fae15a49ec48e68d752152b186e585c
Andy Gill <andygill@ku.edu>
2012-01-25 15:26:15 -0600

    Adding more tests, including some color.

--- src/Diagrams/Tests.hs
+++ src/Diagrams/Tests.hs
@@ -7,7 +7,9 @@ module Diagrams.Tests
         ) where
 
 import Diagrams.Prelude
-import Text.Html as H
+import Diagrams.Combinators
+import Text.Html (Html, tr, concatHtml, th, (<<), valign, toHtml, (!), body, table, renderHtml, src, td, bgcolor)
+import qualified Text.Html as H
 
 -----------------------------------------------------------------------
 
@@ -68,7 +70,31 @@ examples =
                 circle 1 ||| hrule 2 ||| circle 1
         , Test "poly-example" $
                 poly_example
+        , Test "star-polygon" $
+                star (StarSkip 3) (regPoly 13 1) # stroke
+        , Test "star-skip" $
+                stroke (star (StarSkip 2) (regPoly 8 1))
+                       ||| strutX 1
+                       ||| stroke (star (StarSkip 3) (regPoly 8 1))
+        , Test "superimposing" $
+                circle 1 `atop` square (sqrt 2)
+        , Test "superimposing-color" $
+                mconcat [ circle 0.1 # fc green
+                        , eqTriangle 1 # scale 0.4 # fc yellow
+                        , square 1 # fc blue
+                        , circle 1 # fc red
+                        ]
+        , Test "juxtaposing1" $
+                beside (20,30) (circle 1 # fc orange) (circle 1.5 # fc purple)
+                        # showOrigin
+        , Test "juxtaposing2" $
+                let d1 = circle 1 # fc red
+                    d2 = square 1 # fc blue
+                in  (d1 ||| d2) ||| strutX 3 ||| ( d1
+                                                   ===
+                                                   d2  )
         ]
+
 
 poly_example = (poly1 ||| strutX 1 ||| poly2) # lw 0.05
   where
