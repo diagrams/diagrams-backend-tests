0d6e790d4a5262b78c3f4fe3178da451373939c0
Brent Yorgey <byorgey@cis.upenn.edu>
2012-10-28 14:22:44 -0400

    add SVG backend

---
+++ tests/SVG.hs
@@ -0,0 +1,23 @@
+module Main where
+
+import qualified Data.ByteString.Lazy as BS
+import           Diagrams.Backend.SVG
+import           Diagrams.Prelude hiding (D, (<.>))
+import           Diagrams.Tests
+import           System.Directory
+import           System.FilePath ((</>), (<.>))
+import           Text.Blaze.Svg.Renderer.Utf8 (renderSvg)
+import           Text.Html as H hiding ((</>))
+
+main = do
+   -- all output is put into the SVG directory
+   createDirectoryIfMissing False prefix
+   -- and run the tests to generate the SVG examples
+   runTests (examples) "svg-index.html" $ \ (Test nm dig) -> do
+     let svg = renderDia SVG (SVGOptions (Dims 200 200)) dig
+     BS.writeFile (name nm) (renderSvg svg)
+     return
+         $ H.image ! [ src (name nm) ]
+  where
+     name nm = prefix </> nm <.> "svg"
+     prefix = "svg"
