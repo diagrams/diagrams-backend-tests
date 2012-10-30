e1989e908f91d0fd21a695b8e6630bce9d46d540
Brent Yorgey <byorgey@cis.upenn.edu>
2012-10-30 17:35:53 -0400

    factor out SVG tester

---
+++ src/Diagrams/Test/SVG.hs
@@ -0,0 +1,21 @@
+module Diagrams.Test.SVG (svgTester) where
+
+import qualified Data.ByteString.Lazy as BS
+import           Diagrams.Backend.SVG
+import           Diagrams.Prelude hiding (D, (<.>))
+import           Diagrams.Tests
+import           System.FilePath ((</>), (<.>))
+import           Text.Blaze.Svg.Renderer.Utf8 (renderSvg)
+import           Text.Html as H hiding ((</>))
+
+svgTester :: (String, Test -> IO Html)
+svgTester =
+  ( "SVG"
+  , \ (Test nm dig) -> do
+      let svg = renderDia SVG (SVGOptions (Dims 200 200)) dig
+      BS.writeFile (name nm) (renderSvg svg)
+      return $ H.image ! [ src (name nm) ]
+  )
+ where
+  name nm = prefix </> nm <.> "svg"
+  prefix = "svg"
--- tests/SVG.hs
+++ tests/SVG.hs
@@ -1,24 +1,9 @@
 module Main where
 
-import qualified Data.ByteString.Lazy as BS
-import           Diagrams.Backend.SVG
-import           Diagrams.Prelude hiding (D, (<.>))
-import           Diagrams.Tests
-import           System.Directory
-import           System.FilePath ((</>), (<.>))
-import           Text.Blaze.Svg.Renderer.Utf8 (renderSvg)
-import           Text.Html as H hiding ((</>))
+import Diagrams.Tests
+import Diagrams.Test.SVG
+import System.Directory
 
 main = do
-   -- all output is put into the SVG directory
-   createDirectoryIfMissing False prefix
-   -- and run the tests to generate the SVG examples
-   runTests (examples) "svg-index.html" $ [("SVG", \ (Test nm dig) -> do
-     let svg = renderDia SVG (SVGOptions (Dims 200 200)) dig
-     BS.writeFile (name nm) (renderSvg svg)
-     return
-         $ H.image ! [ src (name nm) ]
-     )]
-  where
-     name nm = prefix </> nm <.> "svg"
-     prefix = "svg"
+   createDirectoryIfMissing False "svg"
+   runTests (examples) "svg-index.html" $ [svgTester]
