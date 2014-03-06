bc6c803486d0cb5513d18da76cc3410df14e0d53
Brent Yorgey <byorgey@gmail.com>
2014-03-06 11:43:39 -0500

    don't publish pdf tests

--- publish.sh
+++ publish.sh
@@ -1,4 +1,4 @@
 cabal configure -fall \
   && cabal build \
   && ./dist/build/all-test/all-test \
-  && rsync -rcz cairo postscript svg pdf ref all-index.html byorgey@projects.haskell.org:/srv/projects/diagrams/backend-tests
+  && rsync -rcz cairo postscript svg ref all-index.html byorgey@projects.haskell.org:/srv/projects/diagrams/backend-tests
