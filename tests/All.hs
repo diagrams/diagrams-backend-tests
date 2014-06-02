c11bfff0d6a1231a04a0a9d24e165c9be3854f52
Jeffrey Rosenbluth <jeffrey.rosenbluth@gmail.com>
2014-06-01 23:30:22 -0400

    update for rasterific backend

--- tests/All.hs
+++ tests/All.hs
@@ -3,6 +3,7 @@ module Main where
 import           Diagrams.Test.Cairo
 import           Diagrams.Test.Postscript
 import           Diagrams.Test.SVG
+import           Diagrams.Test.Rasterific
 import           Diagrams.Tests
 import           System.Directory
 
@@ -10,5 +11,6 @@ main = do
    createDirectoryIfMissing False "svg"
    createDirectoryIfMissing False "cairo"
    createDirectoryIfMissing False "postscript"
+   createDirectoryIfMissing False "rasterific"
 
-   runTests examples "all-index.html" $ [cairoTester, svgTester, postscriptTester]
+   runTests examples "all-index.html" $ [cairoTester, svgTester, postscriptTester, rasterificTester]
