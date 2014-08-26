acf228208d4814cab4386cb4a69e95fc7ff492f7
jeffreyrosenbluth <jeffrey.rosenbluth@gmail.com>
2014-08-26 17:47:39 -0400

    adjust image magick for RGBA8

--- src/Diagrams/Test/SVG.hs
+++ src/Diagrams/Test/SVG.hs
@@ -19,23 +19,26 @@ svgTester =
   , \ (Test nm dig) -> do
       let svg = renderDia SVG (SVGOptions (Dims 200 200) Nothing) dig
       BS.writeFile (name nm "svg") (renderSvg svg)
-      rawSystem "convert" [name nm "svg", name nm "png"]
+      rawSystem "convert" [name nm "svg", name nm "png32"]
       -- The generated image.
-      img <- readImage $ name nm "png"
+      img <- readImage $ name nm "png32"
       -- The reference image.
       ref <- readImage $ "ref/" ++ nm ++ ".png"
       -- m: is mean squared error, p: is peak signal to noise ration.
       let (m,p) = case (ref, img) of
             (Left _, _) -> error "Image 1 not read"
             (_, Left _) -> error "Image 2 not read"
-            (Right  i1, Right i2) -> compareImages i1 i2
+            (Right  i1, Right i2) -> compareImages (addAlpha i1) (addAlpha i2)
+          addAlpha img= case img of
+            ImageRGB8 i -> ImageRGBA8 $ promoteImage i
+            otherwise -> img
           -- figure and figCaption are new to Html5 and are implemented
           -- in Diagrams.Tests.
           cap = figCaption (toHtml $ "mse: " ++ printf "%5.3e" m 
                                              ++ ", psnr: " 
                                              ++ printf "%4.1f" p
                                              ++ " db")
-      return $ figure $ H.image ! [ src (name nm "png") ] +++ cap
+      return $ figure $ H.image ! [ src (name nm "svg") ] +++ cap
   )
  where
   name nm ext = prefix </> nm <.> ext
