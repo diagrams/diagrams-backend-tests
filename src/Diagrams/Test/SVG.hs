f22433b5fc02fc100b556aed2e75f5b1637ec7bf
Jeffrey Rosenbluth <jeffrey.rosenbluth@gmail.com>
2014-08-26 17:10:56 -0400

    svg

--- src/Diagrams/Test/SVG.hs
+++ src/Diagrams/Test/SVG.hs
@@ -1,10 +1,14 @@
 module Diagrams.Test.SVG (svgTester) where
 
+import           Codec.Picture (readImage, DynamicImage(..), Image(..), PixelRGBA8)
+import           Codec.Picture.Types (dropAlphaLayer, promoteImage)
+import           Diagrams.Compare
 import qualified Data.ByteString.Lazy as BS
 import           Diagrams.Backend.SVG
 import           Diagrams.Prelude hiding (D, (<.>))
 import           Diagrams.Tests
 import           System.FilePath ((</>), (<.>))
+import           System.Process
 import           Text.Blaze.Svg.Renderer.Utf8 (renderSvg)
 import           Text.Html as H hiding ((</>))
 import           Text.Printf (printf)
@@ -14,10 +18,25 @@ svgTester =
   ( "SVG"
   , \ (Test nm dig) -> do
       let svg = renderDia SVG (SVGOptions (Dims 200 200) Nothing) dig
-      BS.writeFile (name nm) (renderSvg svg)
-      let cap = figCaption (toHtml $ "mse: ____ , psnr: ____  db")
-      return $ figure $ H.image ! [ src (name nm) ] +++ cap
+      BS.writeFile (name nm "svg") (renderSvg svg)
+      rawSystem "convert" [name nm "svg", name nm "png"]
+      -- The generated image.
+      img <- readImage $ name nm "png"
+      -- The reference image.
+      ref <- readImage $ "ref/" ++ nm ++ ".png"
+      -- m: is mean squared error, p: is peak signal to noise ration.
+      let (m,p) = case (ref, img) of
+            (Left _, _) -> error "Image 1 not read"
+            (_, Left _) -> error "Image 2 not read"
+            (Right  i1, Right i2) -> compareImages i1 i2
+          -- figure and figCaption are new to Html5 and are implemented
+          -- in Diagrams.Tests.
+          cap = figCaption (toHtml $ "mse: " ++ printf "%5.3e" m 
+                                             ++ ", psnr: " 
+                                             ++ printf "%4.1f" p
+                                             ++ " db")
+      return $ figure $ H.image ! [ src (name nm "png") ] +++ cap
   )
  where
-  name nm = prefix </> nm <.> "svg"
+  name nm ext = prefix </> nm <.> ext
   prefix = "svg"
