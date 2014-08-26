a9c12d381dc91c7ee80f3c2112d635e4beb91a64
Jeffrey Rosenbluth <jeffrey.rosenbluth@gmail.com>
2014-08-26 15:40:29 -0400

    postscript support

--- src/Diagrams/Test/Rasterific.hs
+++ src/Diagrams/Test/Rasterific.hs
@@ -15,15 +15,22 @@ rasterificTester =
   ( "rasterific"
   , \ (Test nm dig) -> do
       renderRasterific (name nm) (Dims 200 200) 100 dig
+      -- The generated image.
       img <- readImage $ name nm
+      -- The reference image.
       ref <- readImage $ "ref/" ++ nm ++ ".png"
+      -- m: is mean squared error, p: is peak signal to noise ration.
       let (m,p) = case (ref, img) of
             (Left _, _) -> error "Image 1 not read"
             (_, Left _) -> error "Image 2 not read"
+            -- All rasterific pngs have an alpha channel. If the
+            -- reference image does not then we add one.
             (Right  i1, Right i2) -> compareImages (addAlpha i1) i2
           addAlpha img= case img of
             ImageRGB8 i -> ImageRGBA8 $ promoteImage i
             otherwise -> img
+          -- figure and figCaption are new to Html5 and are implemented
+          -- in Diagrams.Tests.
           cap = figCaption (toHtml $ "mse: " ++ printf "%5.3e" m 
                                              ++ ", psnr: " 
                                              ++ printf "%4.1f" p
