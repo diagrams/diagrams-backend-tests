41bd0c892bcfb55ff9b1593f6cea0c23ebaa5c8c
Christopher Chalmers <c.chalmers@me.com>
2015-04-01 15:56:37 +0100

    include diagrams-pgf in all tests

--- tests/All.hs
+++ tests/All.hs
@@ -1,16 +1,20 @@
 module Main where
 
 import           Diagrams.Test.Cairo
+import           Diagrams.Test.PGF
 import           Diagrams.Test.Postscript
-import           Diagrams.Test.SVG
 import           Diagrams.Test.Rasterific
+import           Diagrams.Test.SVG
 import           Diagrams.Tests
 import           System.Directory
 
 main = do
-   createDirectoryIfMissing False "svg"
-   createDirectoryIfMissing False "cairo"
-   createDirectoryIfMissing False "postscript"
-   createDirectoryIfMissing False "rasterific"
+  let mkdir = createDirectoryIfMissing False
+  mkdir "cairo"
+  mkdir "pgf"
+  mkdir "postscript"
+  mkdir "rasterific"
+  mkdir "svg"
 
-   runTests examples "all-index.html" $ [cairoTester, svgTester, postscriptTester, rasterificTester]
+  runTests examples "all-index.html" $
+    [cairoTester, pgfTester, postscriptTester, rasterificTester, svgTester]
